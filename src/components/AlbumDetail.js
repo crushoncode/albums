7e7fd0e7e059087de504ebd8a93a454f83f239c0
Serina Ko <serinako08@gmail.com>
2018-10-11 10:43:36 +1100

    pass onPress as props responding to user input

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -30,7 +30,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
