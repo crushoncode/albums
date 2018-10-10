27e66d3f6c993c4af78d5834f0819651b0cc4bcf
Serina Ko <serinako08@gmail.com>
2018-10-10 14:41:24 +1100

    render a list of components

--- src/components/Albumlist.js
+++ src/components/Albumlist.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((res) => this.setState({ albums: res.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => <Text>{album.title}</Text>);
+  }
+
+  render() {
+    console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
+  }
+}
 
 export default AlbumList;
